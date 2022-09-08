d7a82d3879fbdd7729cc7f0dc2005c00109bc432
Eric McLean <eric.mclean@infermedica.com>
2022-09-08 09:24:00 -0400

    updated settings example with an actual example

---
+++ src/settings-example.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Tomasz Gabrysiak @ Infermedica on 06/03/2017.
+ *
+ * Please provide your own app-id and app-key below.
+ * To obtain keys please register at https://developer.infermedica.com.
+ */
+
+ const settings = {
+    'app-id': '[your app id here]]',
+    'app-key': '[your app key here]'
+  };
+  
+  export default settings;
+  
